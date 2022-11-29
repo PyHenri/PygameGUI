c998f3854c68178006356e81ea636161eb2b89db
PyHenri <95569320+PyHenri@users.noreply.github.com>
2022-11-29 21:17:29 +0100

    borderbox, auto height and width

--- resources/textbox.py
+++ resources/textbox.py
@@ -7,7 +7,8 @@ def splitkeep(s, delimiter):
 
 class Text:
 
-    def __init__(self, screen, x, y, height, width, text_, color=(0, 0, 0), font=None, lineSpacing=0):
+    def __init__(self, screen, x, y, text_, width="auto", height="auto", color=(0, 0, 0), font=None, lineSpacing=0, border=2, border_color=(0 , 0 ,0)):
+        self.max_width = None
         self.screen = screen
         self.x = x
         self.y = y
@@ -15,6 +16,8 @@ class Text:
         self.width = width
         self.lineSpacing = lineSpacing
         self.color = color
+        self.border = border
+        self.border_color = border_color
 
         if type(font) != pygame.font.Font and font is not None:  # font variable must be pygame.font.Font type
             raise TypeError
@@ -27,7 +30,11 @@ class Text:
     def format_text(self, text_):
         text = splitkeep(text_, " ")
 
-        max_width = self.width
+        if self.width == "auto":
+            self.max_width = self.screen.get_width() - self.x - self.border
+        else:
+            self.max_width = self.width
+
         line_width = 0
         split_text = []
         line = ""
@@ -40,7 +47,7 @@ class Text:
                 words = [word]
 
             for idx, w in enumerate(words):
-                if line_width + self.font.size(w)[0] > max_width or idx == 1:
+                if line_width + self.font.size(w)[0] > self.max_width - self.border or idx == 1:
                     split_text.append(line)
                     line = w
                     line_width = self.font.size(w)[0]
@@ -51,6 +58,19 @@ class Text:
         return split_text
 
     def blit_text(self):
+        height = 0
+
         for idx, line in enumerate(self.text_):
             l = self.font.render(line, True, self.color)
-            self.screen.blit(l, (self.x, (idx * l.get_height()) + (self.lineSpacing * idx)))
+            y = (idx * l.get_height()) + (self.lineSpacing * idx) + self.y + self.border
+            x = self.x + self.border
+            self.screen.blit(l, (x, y))
+
+        if self.height == "auto":
+            height = y + l.get_height()
+
+        if self.border > 0:
+            if self.height == "auto":
+                pygame.draw.rect(self.screen, self.border_color, (self.x, self.y, self.max_width, height), self.border)
+            else:
+                pygame.draw.rect(self.screen, self.border_color, (self.x, self.y, self.max_width, self.height), self.border)
